d6d85044b1a03eb9c3c2e687ca68d4321f5461e5
mvuqar1 <vuqar.5100@mail.ru>
2023-07-19 17:58:50 +0400

    tailwind

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -63,7 +63,7 @@ export default function Navbar() {
                 onClick={clearSearch}
               />
             </div>
-            <button type="submit" className="h-10 w-5 md:w-16 flex items-center justify-center bg-zinc-800">
+            <button type="submit" className="h-10 w-7 md:w-16 flex items-center justify-center bg-zinc-800">
               <AiOutlineSearch className="text-xl" onClick={handleSearch} />
             </button>
           </div>
